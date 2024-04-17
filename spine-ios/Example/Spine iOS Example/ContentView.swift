9d9ab195f4131c0969623cd6b3b471d488ce3075
Denis Andrasec <denrase@gmail.com>
2024-04-17 14:24:26 +0200

    Add spineboy.mes sample and read data

--- spine-ios/Example/Spine iOS Example/ContentView.swift
+++ spine-ios/Example/Spine iOS Example/ContentView.swift
@@ -10,6 +10,22 @@ import BaseViews
 import Spine
 
 struct ContentView: View {
+    
+    init() {
+        do {
+            let meshUrl = Bundle.main.url(forResource: "spineboy", withExtension: "mesh")!
+            let imageUrl = Bundle.main.url(forResource: "spineboy", withExtension: "png")!
+            
+            let mesh = try String(contentsOf: meshUrl, encoding: .utf8)
+            let imageData = try Data(contentsOf: imageUrl)
+            
+            let image = UIImage(data: imageData)!
+            loadVertices(mesh: mesh, texture: image)
+        } catch {
+            print(error)
+        }
+    }
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -18,6 +34,37 @@ struct ContentView: View {
             Text("Hello, \(Spine.name)!")
         }
         .padding()
+    }
+    
+    func loadVertices(mesh: String, texture: UIImage) {
+        let lines = mesh.split(separator: "\n")
+        let numVertices = Int(lines[0])!;
+        let numIndices = Int(lines[1])!;
+        
+        var positions = [Float32](repeating: 0, count: numVertices * 2)
+        var uvs = [Float32](repeating: 0, count: numVertices * 2)
+        var colors = [Int32](repeating: 0, count: numVertices)
+        var indices = [UInt16](repeating: 0, count: numVertices)
+        
+        var idx = 2
+        for i in 0..<numVertices * 2 {
+            positions[i] = Float32(lines[idx])! * 0.2
+            idx += 1
+        }
+        for i in 0..<numVertices * 2 {
+            uvs[i] = Float32(lines[idx])! * (i % 2 == 0 ? Float32(texture.size.width) : Float32(texture.size.height));
+            idx += 1
+        }
+        for i in 0 ..< numVertices {
+            colors[i] = Int32(lines[idx])!;
+            idx += 1
+        }
+        for i in 0 ..< numVertices {
+            indices[i] = UInt16(lines[idx])!;
+            idx += 1
+        }
+        
+        print("foo")
     }
 }
 
