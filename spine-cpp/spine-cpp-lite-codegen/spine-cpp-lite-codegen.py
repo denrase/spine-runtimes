2c31f622bdbae10cfa475c90436c6f13240b7f01
Denis Andrasec <denrase@gmail.com>
2024-04-25 11:34:31 +0200

    move to function

--- spine-cpp/spine-cpp-lite-codegen/spine-cpp-lite-codegen.py
+++ spine-cpp/spine-cpp-lite-codegen/spine-cpp-lite-codegen.py
@@ -9,10 +9,13 @@ with open('./spine-cpp-lite/spine-cpp-light.h', 'r') as file:
 # for file_line in file_lines:
 #     print("FOO {}".format(file_line))
 
-types_start = file_contents.find('// parse_start: spine_opaque_types') + len('// parse_start: spine_opaque_types')
-types_end = file_contents.find('// parse_end: spine_opaque_types')
-types_section = file_contents[types_start:types_end]
-types = re.findall(r'SPINE_OPAQUE_TYPE\(([^)]+)\)', types_section)
+def extract_spine_types(data):
+    types_start = data.find('// parse_start: spine_opaque_types') + len('// parse_start: spine_opaque_types')
+    types_end = data.find('// parse_end: spine_opaque_types')
+    types_section = data[types_start:types_end]
+    return re.findall(r'SPINE_OPAQUE_TYPE\(([^)]+)\)', types_section)
+
+types = extract_spine_types(file_contents)
 
 for type in types:
     print(type)
